dd9a13234e0bd331f9c841bebfcc8caf9c4f4822
demeritbird <lekjiewei@hotmail.sg>
2023-08-15 10:10:02 +0800

    feat: generate sections for base-layout

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BaseLayoutComponent } from './base-layout/base-layout.component';
+import { HeroSectionComponent } from './sections/hero-section/hero-section.component';
+import { AboutSectionComponent } from './sections/about-section/about-section.component';
+import { ExperienceProjectsSectionComponent } from './sections/experience-projects-section/experience-projects-section.component';
+import { ArtworksSectionComponent } from './sections/artworks-section/artworks-section.component';
+import { FooterSectionComponent } from './sections/footer-section/footer-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseLayoutComponent,
+    HeroSectionComponent,
+    AboutSectionComponent,
+    ExperienceProjectsSectionComponent,
+    ArtworksSectionComponent,
+    FooterSectionComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/sections/artworks-section/artworks-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artworks-section',
+  templateUrl: './artworks-section.component.html',
+  styleUrls: ['./artworks-section.component.scss']
+})
+export class ArtworksSectionComponent {
+
+}
