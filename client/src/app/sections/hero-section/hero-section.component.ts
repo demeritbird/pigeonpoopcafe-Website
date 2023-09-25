5aac9615c96b5a41be07fab59b4f5a9cc25cb3dd
demeritbird <lekjiewei@hotmail.sg>
2023-09-25 09:29:22 +0800

    feat: implement intersection observer in hero for sticky-nav

--- client/src/app/sections/hero-section/hero-section.component.ts
+++ client/src/app/sections/hero-section/hero-section.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-hero-section',
   templateUrl: './hero-section.component.html',
-  styleUrls: ['./hero-section.component.scss']
+  styleUrls: ['./hero-section.component.scss'],
 })
 export class HeroSectionComponent {
+  @ViewChild('hero_ref', { static: true }) heroView!: ElementRef;
+  STICKY_THRESHOLD: number = 0.2;
 
+  ngOnInit() {
+    this.initStickyObserver();
+  }
+
+  initStickyObserver() {
+    const obsCallback: IntersectionObserverCallback = function (
+      entries: IntersectionObserverEntry[],
+      observer: IntersectionObserver
+    ) {
+      // TODO: Add sticky navigation callback
+    };
+    const options: IntersectionObserverInit = {
+      root: null,
+      threshold: this.STICKY_THRESHOLD,
+    };
+
+    const observer = new IntersectionObserver(obsCallback, options);
+    observer.observe(this.heroView.nativeElement);
+  }
 }
