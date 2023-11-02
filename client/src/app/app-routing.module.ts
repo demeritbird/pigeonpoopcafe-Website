bfc370d7dfdb14e6409c84b84df5cf527e73e1cf
demeritbird <lekjiewei@hotmail.sg>
2023-11-02 12:49:53 +0800

    refactor!: split client file structure into modules

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'restricted',
+    loadChildren: () =>
+      import('./modules/restricted/restricted.module').then(
+        (m) => m.RestrictedModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ client/src/app/modules/main/components/link-icon-tray/link-icon-tray.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+import { MailerService } from '../../services/mailer.service';
+
+@Component({
+  selector: 'app-link-icon-tray',
+  templateUrl: './link-icon-tray.component.html',
+  styleUrls: ['./link-icon-tray.component.scss'],
+})
+export class LinkIconTrayComponent {
+  @Input() hasBound: boolean = false;
+
+  constructor(private mailerService: MailerService) {}
+
+  modalOpen: boolean = false;
+  onModalToggle() {
+    this.modalOpen = !this.modalOpen;
+  }
+
+  email: string = '';
+  onModalSubmit() {
+    this.mailerService.requestMail(this.email).subscribe();
+    this.onModalToggle();
+  }
+
+  onMailIconClick() {
+    this.onModalToggle();
+  }
+}
