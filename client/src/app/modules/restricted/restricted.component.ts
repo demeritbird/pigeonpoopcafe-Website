f396b0342b056f8393d29b92c343fd257dec43e3
demeritbird <lekjiewei@hotmail.sg>
2024-08-04 14:07:00 +0800

    feat: restricted screen enlarge on auth

--- client/src/app/modules/restricted/restricted.component.ts
+++ client/src/app/modules/restricted/restricted.component.ts
@@ -6,6 +6,12 @@ import {
   transition,
   trigger,
 } from '@angular/animations';
+import {
+  AuthService,
+  CurrentUser,
+} from './submodules/auth/services/auth.service';
+import { BehaviorSubject, filter, Subscription } from 'rxjs';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-restricted',
@@ -19,11 +25,41 @@ import {
   ],
 })
 export class RestrictedComponent {
+  routerSubscription!: Subscription;
+  currentRoute: string | null = null;
+  currentUser$: BehaviorSubject<CurrentUser | null>;
+  isOnRestrictedPage: boolean = false;
+
   imageIsLoading: boolean = true;
   icons = Array(10);
+
+  constructor(private router: Router, private authService: AuthService) {
+    this.currentUser$ = this.authService.currentUser$;
+  }
+
+  ngOnInit(): void {
+    this.routerSubscription = this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd)) // prevent multiple calls
+      .subscribe(() => {
+        this.isOnRestrictedPage = this.checkIsNestedRestrictedRoute();
+      });
+
+    this.isOnRestrictedPage = this.checkIsNestedRestrictedRoute();
+  }
+
+  private checkIsNestedRestrictedRoute(): boolean {
+    const urlSegments = this.router.url.split('/');
+    return urlSegments.length > 2 && urlSegments[1] === 'restricted';
+  }
 
   imageOnLoad() {
     // lazy loading
     this.imageIsLoading = false;
+  }
+
+  ngOnDestroy(): void {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
   }
 }
